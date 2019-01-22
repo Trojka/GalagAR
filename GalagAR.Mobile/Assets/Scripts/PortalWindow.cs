017ade7e770fc7ad5260275a5bba0930aace6fbb
Serge Desmedt <sergedesmedt@yahoo.com>
2019-01-22 19:08:05 +0100

    Test Portal Implementation
    see https://www.youtube.com/playlist?list=PLKIKuXdn4ZMhwJmPnYI0e7Ixv94ZFPvEP (video's 1 to 4)

---
+++ GalagAR.Mobile/Assets/Scripts/PortalWindow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalWindow : MonoBehaviour {
+
+    public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+        foreach (var material in materials)
+        {
+            material.SetInt("_StencilTest", (int)CompareFunction.Equal);
+        }
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name != "Main Camera")
+            return;
+
+        if(transform.position.z > other.transform.position.z)
+        {
+            foreach(var material in materials)
+            {
+                material.SetInt("_StencilTest", (int)CompareFunction.Equal);
+            }
+        }
+        else
+        {
+            foreach (var material in materials)
+            {
+                material.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        foreach (var material in materials)
+        {
+            material.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
