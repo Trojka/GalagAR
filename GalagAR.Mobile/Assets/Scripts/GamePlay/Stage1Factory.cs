0ddd6ad5ceaa45d0122b49539f4056d092da800e
Serge Desmedt <sergedesmedt@yahoo.com>
2019-03-12 21:03:22 +0100

    Stage1: Positioning

--- GalagAR.Mobile/Assets/Scripts/GamePlay/Stage1Factory.cs
+++ GalagAR.Mobile/Assets/Scripts/GamePlay/Stage1Factory.cs
@@ -6,6 +6,8 @@ public class Stage1Factory : MonoBehaviour {
 
     public Transform pathT1_1;
 
+    public Transform endPointHolder;
+
     public Transform t1_1_End;
     public Transform t1_2_End;
     public Transform t1_3_End;
@@ -27,30 +29,49 @@ public class Stage1Factory : MonoBehaviour {
     const int endPointIndex = 9;
 
     void CreateStage1Wave1Paths() {
+
+        Debug.Log("endPointHolder.position: " + endPointHolder.position);
+        Debug.Log("endPointHolder.localPosition: " + endPointHolder.localPosition);
+
+
         pathT1_2 = Instantiate(pathT1_1, this.transform, false);
         pathT1_2.name = "PathT1_2";
-        //pathT1_3 = Instantiate(pathT1_1);
-        //pathT1_4 = Instantiate(pathT1_1);
+        pathT1_3 = Instantiate(pathT1_1, this.transform, false);
+        pathT1_3.name = "PathT1_3";
+        pathT1_4 = Instantiate(pathT1_1, this.transform, false);
+        pathT1_3.name = "PathT1_3";
 
         pathT2_1 = Instantiate(pathT1_1, this.transform, false);
         pathT2_1.name = "PathT2_1";
         pathT2_1.transform.localScale = new Vector3(1, 1, -1);
 
-        //pathT2_2 = Instantiate(pathT2_1);
-        //pathT2_3 = Instantiate(pathT2_1);
-        //pathT2_4 = Instantiate(pathT2_1);
+        pathT2_2 = Instantiate(pathT2_1, this.transform, false);
+        pathT2_2.name = "PathT2_2";
+        pathT2_3 = Instantiate(pathT2_1, this.transform, false);
+        pathT2_3.name = "PathT2_3";
+        pathT2_4 = Instantiate(pathT2_1, this.transform, false);
+        pathT2_4.name = "PathT2_4";
 
-        pathT1_1.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, this.transform.InverseTransformPoint(t1_1_End.position));
+        // tranform from worldspace to localspace
+        var t1_1_EndLocal = this.transform.InverseTransformPoint(t1_1_End.position);
+        Debug.Log("t1_1_End: " + t1_1_End.position);
+        Debug.Log("t1_1_EndLocal: " + t1_1_EndLocal);
+        pathT1_1.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, t1_1_EndLocal);
         pathT1_2.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, this.transform.InverseTransformPoint(t1_2_End.position));
 
-        //pathT1_3.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, t1_3_End.position);
-        //pathT1_4.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, t1_4_End.position);
+        pathT1_3.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, this.transform.InverseTransformPoint(t1_3_End.position));
+        pathT1_4.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, this.transform.InverseTransformPoint(t1_4_End.position));
 
-        pathT2_1.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, this.transform.InverseTransformPoint(t2_1_End.position));
+        var t2_1_EndLocal = this.transform.InverseTransformPoint(t2_1_End.position);
+        var t2_1_EndLocalScaled = Vector3.Scale(new Vector3(1, 1, -1), t2_1_EndLocal);
+        Debug.Log("t2_1_End: " + t2_1_End.position);
+        Debug.Log("t2_1_EndLocal: " + t2_1_EndLocal);
+        Debug.Log("t2_1_EndLocalScaled: " + t2_1_EndLocalScaled);
+        pathT2_1.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, t2_1_EndLocalScaled);
 
-        //pathT2_2.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, t2_2_End.position);
-        //pathT2_3.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, t2_3_End.position);
-        //pathT2_4.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, t2_4_End.position);
+        pathT2_2.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, Vector3.Scale(new Vector3(1, 1, -1), this.transform.InverseTransformPoint(t2_2_End.position)));
+        pathT2_3.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, Vector3.Scale(new Vector3(1, 1, -1), this.transform.InverseTransformPoint(t2_3_End.position)));
+        pathT2_4.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, Vector3.Scale(new Vector3(1, 1, -1), this.transform.InverseTransformPoint(t2_4_End.position)));
     }
 
 	// Use this for initialization
