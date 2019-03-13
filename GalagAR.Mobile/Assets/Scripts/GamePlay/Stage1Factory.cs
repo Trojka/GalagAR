fddde40c3e7c9b7b3d5975ddab85e165826c15ee
Serge Desmedt <sergedesmedt@yahoo.com>
2019-03-13 20:57:57 +0100

    Stage1: pulsing enemy creation

--- GalagAR.Mobile/Assets/Scripts/GamePlay/Stage1Factory.cs
+++ GalagAR.Mobile/Assets/Scripts/GamePlay/Stage1Factory.cs
@@ -20,6 +20,12 @@ public class Stage1Factory : MonoBehaviour {
     public Transform t2_4_End;
 
     public Transform EnemyType1;
+    public int MaxNumberOfType1Enemies;
+    List<BezierSpline> enemyType1Paths;
+    int numberOfType1Enemies;
+
+    public float CreateEnemyEverySeconds;
+    float progress;
 
     Transform pathT1_2;
     Transform pathT1_3;
@@ -76,19 +82,38 @@ public class Stage1Factory : MonoBehaviour {
         pathT2_2.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, Vector3.Scale(new Vector3(1, 1, -1), this.transform.InverseTransformPoint(t2_2_End.position)));
         pathT2_3.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, Vector3.Scale(new Vector3(1, 1, -1), this.transform.InverseTransformPoint(t2_3_End.position)));
         pathT2_4.GetComponent<BezierSpline>().SetControlPoint(endPointIndex, Vector3.Scale(new Vector3(1, 1, -1), this.transform.InverseTransformPoint(t2_4_End.position)));
+
+        enemyType1Paths = new List<BezierSpline>();
+        enemyType1Paths.Add(pathT1_1.GetComponent<BezierSpline>());
+        enemyType1Paths.Add(pathT1_2.GetComponent<BezierSpline>());
+        enemyType1Paths.Add(pathT1_3.GetComponent<BezierSpline>());
+        enemyType1Paths.Add(pathT1_4.GetComponent<BezierSpline>());
     }
 
 	// Use this for initialization
 	void Start () {
         CreateStage1Wave1Paths();
-
-        var et1 = Instantiate(EnemyType1);
-        et1.GetComponent<SplineWalker>().spline = pathT1_1.GetComponent<BezierSpline>();
-        et1.GetComponent<SplineWalker>().Walk();
+        progress = 0;
+        numberOfType1Enemies = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        progress += Time.deltaTime;
+        if((numberOfType1Enemies < MaxNumberOfType1Enemies) && (progress > CreateEnemyEverySeconds)) {
+            var et1 = CreateEnemyType1(numberOfType1Enemies);
+            et1.Walk();
+
+            numberOfType1Enemies++;
+            progress -= CreateEnemyEverySeconds;
+        }
 	}
+
+    SplineWalker CreateEnemyType1(int enemyNumber) {
+        var et1 = Instantiate(EnemyType1);
+        et1.GetComponent<SplineWalker>().spline = enemyType1Paths[enemyNumber];
+
+        return et1.GetComponent<SplineWalker>();
+    }
 }
